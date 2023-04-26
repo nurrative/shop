3390865eabb1a447f000ad9962907ef0e3a5fc76
nurrative <nursul1997@gmail.com>
2023-04-26 11:06:23 +0600

    MVC for main

---
+++ main/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ProductViewSet, CategoryListCreateAPIView, CategoryDestroyAPIView
+
+router = DefaultRouter()
+router.register('products', ProductViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('categories/', CategoryListCreateAPIView.as_view()),
+    path('categories/<int:pk>/', CategoryDestroyAPIView().as_view()),
+]
--- main/views.py
+++ main/views.py
@@ -1,2 +1,19 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import ListCreateAPIView, DestroyAPIView
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class CategoryListCreateAPIView(ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class CategoryDestroyAPIView(DestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
