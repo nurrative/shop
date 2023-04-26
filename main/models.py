fc259c6fc2780778c08fa183efa25a33e0d385f3
nurrative <nursul1997@gmail.com>
2023-04-26 10:50:27 +0600

    crud main

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    title = models.CharField(max_length=150)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    quantity = models.IntegerField()
+
