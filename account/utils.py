14e48ee8128aa3dcb09dcae0d48d5d923c3acf92
nurrative <nursul1997@gmail.com>
2023-05-11 10:56:28 +0600

    allowed_hosts

--- account/utils.py
+++ account/utils.py
@@ -1,11 +1,11 @@
 from django.core.mail import send_mail
-
+from decouple import config
 
 def send_activation_code(email: str, code: str):
  message = ""
  html = f"""
 <h1>для активации нажмите на кнопку</h1>
-<a href="http://127.0.0.1:8000/api/v1/account/activate/{code}">
+<a href="{config('LINK')}api/v1/account/activate/{code}">
 <button>Activate</button>
 </a>
 """
